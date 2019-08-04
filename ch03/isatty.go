84d23583966cc08b1ee925855ed83076ac140710
noissefnoc <noissefnoc@gmail.com>
2019-08-04 18:38:55 +0900

    add isatty sample.

---
+++ ch03/isatty.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/mattn/go-isatty"
+	"io"
+	"os"
+	"strings"
+)
+
+type flusher interface {
+	Flush() error
+}
+
+func main() {
+	var output io.Writer
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		// if output is a terminal
+		output = os.Stdout
+	} else {
+		// wrap bufio.Writer
+		output = bufio.NewWriter(os.Stdout)
+	}
+	for i := 0; i < 100; i++ {
+		fmt.Fprintln(output, strings.Repeat("x", 100))
+	}
+	if _o, ok := output.(flusher); ok {
+		_o.Flush()
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/noissefnoc/go-for-everyone-2nd-edition
 
 go 1.12
 
-require github.com/rakyll/statik v0.1.6
+require (
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/rakyll/statik v0.1.6
+)
