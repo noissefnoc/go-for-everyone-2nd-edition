ab2e473d321e9b173a138758a3e8ff5d2acf7cb6
noissefnoc <noissefnoc@gmail.com>
2019-08-10 23:33:49 +0900

    finish implement list 40

--- ch03/simple_signal_handling.go
+++ ch03/simple_signal_handling.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -21,4 +22,26 @@ func main() {
 	signal.Notify(sigCh, trapSignals...)
 
 	// create cancelable context for passing main processing
+	ctx, cancel := context.WithCancel(context.Background())
+	// wait signal another goroutine
+	go func() {
+		// block until catch signal
+		sig := <-sigCh
+		fmt.Println("Got signal", sig)
+		cancel()
+	}()
+	doMain(ctx)
+}
+
+func doMain(ctx context.Context) {
+	defer fmt.Println("done doMain")
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			// do something
+		}
+		// do something
+	}
 }
