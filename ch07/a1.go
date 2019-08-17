d2dca72d9a347c493d4af90e077b46cdb6e015b5
noissefnoc <noissefnoc@gmail.com>
2019-08-17 18:07:26 +0900

    finished reading 7.6

--- ch07/a1.go
+++ ch07/a1.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"database/sql"
+	"github.com/go-gorp/gorp"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
+type Comment struct {
+	Id int64 `db:"id,primarykey,autoincrement"`
+	Name string `db:"name,notnull,default:'名無し',size:200"`
+	Text string `db:"text,notnull,size:400"`
+	Created time.Time `db:"created,notnull"`
+	Updated time.Time `db:"updated,notnull"`
+}
+
+func (c *Comment) PreInsert(s gorp.SqlExecutor) error {
+	c.Created = time.Now()
+	c.Updated = c.Created
+	return nil
+}
+
+func (c *Comment) PreUpdate(s gorp.SqlExecutor) error {
+	c.Updated = time.Now()
+	return nil
+}
+
 func main() {
+	/*
 	dsn := os.Getenv("DSN")
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
@@ -35,4 +57,36 @@ func main() {
 	}
 
 	log.Printf("affected: %d, lastInsertedID: %d\n", affected, lastInsertedID)
+	 */
+
+	dsn := os.Getenv("DSN")
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+	dbmap.AddTableWithName(Comment{}, "comments").
+		SetKeys(true, "id").
+		AddIndex("idx_comments", "Btree", []string{"id"}).
+		SetUnique(true)
+	err = dbmap.CreateTablesIfNotExists()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = dbmap.Insert(&Comment{Text:"こんにちわ"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbmap.Insert(&Comment{Name:"bob", Text:"こんにちわ"})
+	var comment Comment
+	dbmap.SelectOne(&comment, "SELECT * FROM comments WHERE id = 1")
+
+	comment.Text = "こんばんわ"
+	dbmap.Update(&comment)
+
+	var comments []Comment
+	dbmap.Select(&comments, "SELECT * FROM comments WHERE name = $1", "bob")
 }
